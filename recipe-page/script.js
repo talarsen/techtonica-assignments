0ea7b823c579d3a478cf6d0b42653b125d72c8bf
Tara Larsen <talarsen74@gmail.com>
2021-07-20 13:32:22 -0700

    initial commit of script.js

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -1,1 +1,15 @@
-/*******This is the JS script for my recipe page */
+//grabbing the div that has the ingredient list and index 0 avoids HTMLCollection
+const divs = document.getElementsByClassName("ingredientsList");
+console.log(divs[0])//just gives one div
+
+const lis = divs[0].getElementsByTagName("li");
+  for (let li of lis) {
+    console.log(li.innerHTML)
+  }
+
+
+// lis.addEventListener("click", );
+// .text-decoration-line-through
+
+
+const logoClick = document
