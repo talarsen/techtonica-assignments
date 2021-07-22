21ff2f8a7e704b92e5f1816b37477cd2695ac3bf
Tara Larsen <talarsen74@gmail.com>
2021-07-22 08:18:54 -0700

    change ingredient list item one to blue color

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -1,15 +1,28 @@
-//grabbing the div that has the ingredient list and index 0 avoids HTMLCollection
-const divs = document.getElementsByClassName("ingredientsList");
-console.log(divs[0])//just gives one div
+// //grabbing the div that has the ingredient list and index 0 avoids HTMLCollection
+// const divs = document.getElementsByClassName("ingredientsList");
+// console.log(divs)//just gives one div
 
-const lis = divs[0].getElementsByTagName("li");
-  for (let li of lis) {
-    console.log(li.innerHTML)
-  }
-
-
-// lis.addEventListener("click", );
-// .text-decoration-line-through
+// const lis = divs[0].getElementsByTagName("li");
+//   for (let li of lis) {
+//     console.log(li.innerHTML)
+//   }
 
 
-const logoClick = document
+// // lis.addEventListener("click", );
+// // .text-decoration-line-through
+
+// const li = document.getElementById("ingredient1")
+// console.log(li);
+// li.style.color = "blue";
+
+//grab each li and log it t the console
+const lis = document.getElementsByClassName("ingredients");
+for (let li of lis) {
+   
+   console.log(li.innerHTML)
+}
+
+lis.addEventListener("click", function () {
+   li.style.textDecoration = "line-through" 
+})
+
