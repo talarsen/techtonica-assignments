1fec55d4b5dfd59bd4fe64364959ddb60833104d
Tara Larsen <talarsen74@gmail.com>
2021-07-26 09:58:59 -0700

    update recipe page file structure

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -1,28 +1,59 @@
-// //grabbing the div that has the ingredient list and index 0 avoids HTMLCollection
-// const divs = document.getElementsByClassName("ingredientsList");
-// console.log(divs)//just gives one div
-
-// const lis = divs[0].getElementsByTagName("li");
-//   for (let li of lis) {
-//     console.log(li.innerHTML)
-//   }
-
-
-// // lis.addEventListener("click", );
-// // .text-decoration-line-through
 
 // const li = document.getElementById("ingredient1")
-// console.log(li);
+// //console.log(li);
 // li.style.color = "blue";
 
-//grab each li and log it t the console
-const lis = document.getElementsByClassName("ingredients");
-for (let li of lis) {
-   
-   console.log(li.innerHTML)
+
+
+const toggleV2 = (e) => {
+   const li = e.target;//what dom element is the event happening to
+   li.classList.toggle("obtains")
+   //https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/
 }
 
-lis.addEventListener("click", function () {
-   li.style.textDecoration = "line-through" 
-})
 
+document.querySelectorAll(".ingredientList li").forEach(li => {
+   li.addEventListener("click", toggleV2)
+});
+
+
+
+
+
+//grab each li and log it to the console
+
+// const lis = document.getElementsByClassName("ingredients");
+
+// for (let li of lis) {
+//    return li.innerHTML
+//    // console.log(li.innerHTML)
+// }
+// let crossOutIngredientList = function() {
+//    for (let li of lis) {
+//       return li.innerHTML
+//      let finalLine=  lis.style.textDecoration = "line-through";
+//       // console.log(li.innerHTML)
+//    }  
+//    return finalLine
+// }
+// crossOutIngredientList.addEventListener("mouseover", crossOutIngredientList)
+
+    // 1. Find and store the element we want to listen to events on.
+   let delishButton = document.getElementById("clicker");
+    // 2. Define the function that will respond to the event.
+    // callback 
+    let onButtonClick = function() {
+      delishButton.textContent = "Oh wow, you clicked me!";
+    };
+    // 3. Add the event listener for the element and function
+    // anonymous "inline"
+    delishButton.addEventListener("click", onButtonClick);
+
+    let recipeLink = function() {
+      let a = document.createElement('a');
+      let linkText = document.createTextNode("Go to Delish.com");
+      a.appendChild(linkText);
+      a.title = "Delish.com";
+      a.href = "https://delish.com";
+      document.body.appendChild(a);
+    };
