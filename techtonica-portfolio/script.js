f18c9f86d0311ba3e243534f35a4e9af29b381f4
Tara Larsen <talarsen74@gmail.com>
2021-07-30 16:31:44 -0700

    initial commit

---
+++ techtonica-portfolio/script.js
@@ -0,0 +1,17 @@
+/*****Contact Form ******/
+
+
+
+function clickHandler(event) {
+  console.log('Button Clicked');
+}
+
+let nameButtonClick = (event) => {
+  console.log("Name is")
+}
+// let nameButton = document.createElement(name)
+// let newtext = document.createTextNode(text)
+//   //newElement.appendChild(newtext)
+//   document.getElementById("divTag").appendChild(newElement)
+//   newElement.appendChild(newtext)
+//   newElement.classList.add(newClass)
